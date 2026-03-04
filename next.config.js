13c2061e6bf163c9ad1c9064afae30337361a5d7
shyam kumar <pshyamkumar55@gmail.com>
2026-03-03 23:06:54 -0500

    updated

--- next.config.js
+++ next.config.js
@@ -3,3 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 };
 module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = require("path").resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
